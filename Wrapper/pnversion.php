62455d8b305ba361818124f13a110404d1595c1d
Christopher X. Candreva <chris@westnet.com>
2009-08-28 20:52:00 +0000

    Change module name, reset version number

--- Wrapper/pnversion.php
+++ Wrapper/pnversion.php
@@ -1,15 +1,15 @@
 <?php
 // $Id: pnversion.php,v 1.1.1.1 2002/09/15 22:26:15 root Exp $
-$modversion['name'] = 'NukeWrapper';
-$modversion['version'] = '2.7.2 beta';
-$modversion['description'] = 'Wraps local and external pages in PostNuke site';
+$modversion['name'] = 'Wrapper';
+$modversion['version'] = '0.0';
+$modversion['description'] = 'Wraps local and external pages in Zikula site';
 $modversion['credits'] = 'documents/credits.txt';
-$modversion['help'] = 'documents/NukeWrapper.html';
+$modversion['help'] = 'documents/Wrapper.html';
 $modversion['changelog'] = 'documents/changelog.txt';
 $modversion['license'] = 'documents/license.txt';
 $modversion['official'] = 0;
 $modversion['author'] = 'Martin Andersen';
 $modversion['contact'] = 'msandersen at tpg.com.au';
 $modversion['admin'] = 1;
-$modversion['securityschema'] = array('NukeWrapper::file|url' => 'filename.ext:keyword:extension');
+$modversion['securityschema'] = array('Wrapper::file|url' => 'filename.ext:keyword:extension');
 ?>
